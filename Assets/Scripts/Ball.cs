7b137d3d6f33cac725bf77cc70b65fd7ec6a6c91
dianacamacho <dcam1901@yahoo.com>
2017-01-02 23:45:01 -0600

    unity game - block breaker

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private Paddle paddle;
+
+	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!hasStarted) {
+			// Lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			// Wait for a mouse press to launch
+			if (Input.GetMouseButtonDown (0)) {
+				print("Mouse clicked, launch ball");
+				hasStarted = true;
+				GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		Vector2 tweak = new Vector2 (Random.Range (-1f, 1f), Random.Range (0f, 0.2f));
+
+		if (hasStarted) {
+			GetComponent<Rigidbody2D>().velocity += tweak;
+			Debug.Log(GetComponent<Rigidbody2D>().velocity);
+			Debug.Log("Speed: " + GetComponent<Rigidbody2D>().velocity.magnitude);
+			if (!(col.gameObject.tag == "Breakable")) {
+				GetComponent<AudioSource> ().Play ();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	private LevelManager levelManager;
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		print("Collision");
+		levelManager.LoadLevel("Lose");
+	}
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		print("Trigger");
+		levelManager.LoadLevel("Lose");
+	}
+}
